fa47ba2b140e2d7e51f24bd7c177caaafda71e4b
Víctor Lara <victor.lara.ortega@ing.com>
2020-12-14 15:04:58 +0100

    feat: remove dirty code and add logs

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const { Gitlab } = require("@gitbeaker/node");
 const { token, host } = require('./vault.js');
 
+// Edit this variables to match your pipeline
 const projectId = "59108";
 const pipelineId = "3186956";
-
-const RESULTS_NUMBER = 20;
 
 const api = new Gitlab({
   host,
@@ -15,11 +14,6 @@ const api = new Gitlab({
 const getPipelineData = async () => {
   const pipelineInfo = await api.Pipelines.show(projectId, pipelineId);
   return pipelineInfo;
-};
-
-const getPipelineJobs = async () => {
-  const pipelineJobs = await api.Pipelines.showJobs(projectId, pipelineId);
-  return pipelineJobs;
 };
 
 const retryPipeline = async () => {
@@ -34,26 +28,23 @@ function sleep(ms) {
 (async () => {
   async function run() {
     const pipelineData = await getPipelineData();
-    // if(pipelineData.status === "running" || pipelineData.status === "failed") {
-    //   const pipelineJobs = await getPipelineJobs();
-    //   const failedJobs = pipelineJobs.filter(job => job.status === 'failed')
-    //   console.log(failedJobs)
-    // }
 
     if (pipelineData.status === "running") {
       console.log("Pipeline running, waiting one minute to recheck status");
+      // retry in 1 minute
       await sleep(60000)
       await run();
-      // retry in 1 minute
+      
     }
-    if (pipelineData.status === "failed") {
+    else if (pipelineData.status === "failed") {
       console.log("Pipeline failed!, retrying...");
-
-      const retryData = await retryPipeline()
-      // console.log(retryData)
+      await retryPipeline()
       console.log("Retryied")
       await sleep(6000)
       await run();
+    } else {
+      console.log(`Pipeline in status ${pipelineData.status}, finishing the loop.`);
+
     }
   }
 
